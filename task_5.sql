fa4dcb1dc34da5319e33e76c834bedec1db2e86e
Mohan <mohansang04@gmail.com>
2024-07-28 22:58:24 +0300

    fixed bugs

--- task_5.sql
+++ task_5.sql
@@ -2,7 +2,7 @@ USE alx_book_store;
 
 -- RENAME TABLE Customers TO Customer;
 
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
 ON DUPLICATE KEY UPDATE
 customer_name = VALUES(customer_name),
