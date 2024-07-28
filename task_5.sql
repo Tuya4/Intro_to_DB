af38d8da38d8face4f79feb93f3c82694eb395e4
Mohan <mohansang04@gmail.com>
2024-07-28 22:50:01 +0300

    update

--- task_5.sql
+++ task_5.sql
@@ -1,11 +1,6 @@
-USE alx_book_store;
+-- USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS Customer (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT
-);
+-- RENAME TABLE Customers TO Customer;
 
 INSERT INTO Customer IF NOT EXISTS (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
